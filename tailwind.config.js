0b8d334e8e935381564aa3dacc9fe3108c77dd5b
fmorar <fabianmorar1223@gmail.com>
2023-01-03 09:31:39 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "gray-primary": "#979BA3",
+        "primary": "#494666",
+        "dark-primary": "#353535"
+      },
+    },
+  },
+  plugins: [],
+}

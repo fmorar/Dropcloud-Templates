7f57006c7399a26f55b204ea6cdc924595056dd7
fmorar <fabianmorar1223@gmail.com>
2023-01-05 09:25:26 -0300

    login and reset page implementation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,9 @@ module.exports = {
       colors: {
         "gray-primary": "#979BA3",
         "primary": "#494666",
-        "dark-primary": "#353535"
+        "dark-primary": "#353535",
+        "dark-modal-primary": "#283555",
+        "gray-modal-primary": "#65646E" 
       },
     },
   },

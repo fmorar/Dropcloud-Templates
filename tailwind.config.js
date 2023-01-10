867ef9f485a7f09c703610150d60dbbe54e4e9c2
fmorar <fabianmorar1223@gmail.com>
2023-01-10 10:00:29 -0300

    10-01: Implement create organization form

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,10 @@ module.exports = {
         "primary": "#494666",
         "dark-primary": "#353535",
         "dark-modal-primary": "#283555",
-        "gray-modal-primary": "#65646E" 
+        "gray-modal-primary": "#65646E",
+        "sidebar": "#F7FAFD",
+        "sidebar-options": "#121627",
+        "sidebar-hover": "#949494"
       },
     },
   },

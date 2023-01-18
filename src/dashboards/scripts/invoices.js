4de97cc7c0d47423f45f032edcb4f60bfb0269eb
fmorar <fabianmorar1223@gmail.com>
2023-01-18 15:24:51 -0300

    18-01: Add functionality to receipts dashboard

--- src/dashboards/scripts/invoices.js
+++ src/dashboards/scripts/invoices.js
@@ -50,7 +50,7 @@ function updateThumbnail(dropZoneElement, file) {
     /*first time there won't be a thumbnailElement so it has to be created*/
     if (!thumbnailElement) {
         thumbnailElement = document.createElement("div");
-        thumbnailElement.classList.add("drop-zone__thumb","w-full", "h-full", "rounded-md", "overflow-hidden", "bg-gray-300", "bg-cover", "relative", "after:content-[attr(data-label)]", "after:absolute", "after:bottom-0" , "after:left-0", "after:w-full", "after:p-5", "after:text-white", "after:bg-black", "after:bg-opacity-75", "after:text-center", "after:font-size-14");
+        thumbnailElement.classList.add("drop-zone__thumb", "w-full", "h-full", "rounded-md", "overflow-hidden", "bg-gray-300", "bg-cover", "relative", "after:content-[attr(data-label)]", "after:absolute", "after:bottom-0", "after:left-0", "after:w-full", "after:p-5", "after:text-white", "after:bg-black", "after:bg-opacity-75", "after:text-center", "after:font-size-14");
         dropZoneElement.appendChild(thumbnailElement);
     }
     thumbnailElement.dataset.label =
@@ -68,3 +68,77 @@ function updateThumbnail(dropZoneElement, file) {
         thumbnailElement.style.backgroundImage = null; /*plain background for non image type files*/
     }
 }
+
+(function () {
+
+    // When the endpoint is available, use this 
+/*function getData() {
+    fetch("https://my-mock-endpoint.com/data")
+      .then((response) => response.json())
+      .then((data) => {
+          let table = document.getElementById("myTable").getElementsByTagName("tbody")[0];
+          data.forEach(function(rowData) {
+            var newRow = table.insertRow(table.length);
+            var fileCell = newRow.insertCell(0);
+            var adminCell = newRow.insertCell(1);
+            var commentCell = newRow.insertCell(2);
+            var statusCell = newRow.insertCell(3);
+            var actionsCell = newRow.insertCell(4);
+
+            fileCell.innerHTML = rowData.fileName
+            adminCell.innerHTML = rowData.adminName
+            commentCell.innerHTML = rowData.comment
+            statusCell.innerHTML = rowData.status
+            actionsCell.innerHTML = "<button onclick='deleteRow(this)'>Delete</button>";
+          });
+      })
+      .catch((error) => console.log(error));
+  } 
+  document.addEventListener("DOMContentLoaded", function(event) { 
+    getData()
+  });
+  
+  */
+ 
+    let mockData = [
+        {
+            fileName: "INV-1", adminName: "Wade Warren",
+            comment: "Payment for logistics costs", status: "Processed"
+        },
+        {
+            fileName: "INV-2", adminName: "Brooklyn Simpsons",
+            comment: "Payment for office costs", status: "Pending"
+        },
+    ]
+
+    function getData() {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve(mockData);
+            }, 1000);
+        });
+    }
+
+
+    getData()
+        .then((data) => {
+            let table = document.getElementById("myTable").getElementsByTagName("tbody")[0];
+            data.forEach(function (rowData) {
+                var newRow = table.insertRow(table.length);
+                var fileCell = newRow.insertCell(0);
+                var adminCell = newRow.insertCell(1);
+                var commentCell = newRow.insertCell(2);
+                var statusCell = newRow.insertCell(3);
+                var actionsCell = newRow.insertCell(4);
+
+                fileCell.innerHTML = rowData.fileName
+                adminCell.innerHTML = rowData.adminName
+                commentCell.innerHTML = rowData.comment
+                statusCell.innerHTML = rowData.status
+                actionsCell.innerHTML = "<button onclick='deleteRow(this)'>Delete</button>";
+            });
+        })
+        .catch((error) => console.log(error));
+
+
+})();

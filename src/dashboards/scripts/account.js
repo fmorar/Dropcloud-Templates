0b63853ea708fcc60912921289a29a1961cdedf9
fmorar <fabianmorar1223@gmail.com>
2023-02-08 14:39:08 -0300

    8-02:Implemented account settings tabs content

--- src/dashboards/scripts/account.js
+++ src/dashboards/scripts/account.js
@@ -14,13 +14,37 @@ tabTogglers.forEach(function(toggler) {
 
     for (let i = 0; i < tabContents.children.length; i++) {
       
-      tabTogglers[i].parentElement.classList.remove("border-b");  tabContents.children[i].classList.remove("hidden");
+      tabTogglers[i].parentElement.classList.remove("border-b-[3px]");  tabContents.children[i].classList.remove("hidden");
       if ("#" + tabContents.children[i].id === tabName) {
         continue;
       }
       tabContents.children[i].classList.add("hidden");
       
     }
-    e.target.parentElement.classList.add("border-b");
+    e.target.parentElement.classList.add("border-b-[3px]");
   });
+});
+
+var parent = document.querySelector(".parent");
+
+// password show/hide helper function
+function showHide(input, showText) {
+    if (input.getAttribute("type") === "password") {
+        input.setAttribute("type", "text");
+        showText.classList.remove("fa-eye");
+        showText.classList.add("fa-eye-slash");
+    } else {
+        input.setAttribute("type", "password");
+        showText.classList.remove("fa-eye-slash");
+        showText.classList.add("fa-eye");
+    }
+}
+
+// event delegation on event target match
+parent.addEventListener("click", event => {
+    if (event.target.matches("i")) {
+        var spanElm = event.target;
+        var inputElm = spanElm.previousElementSibling;
+        showHide(inputElm, spanElm);
+    }
 });
